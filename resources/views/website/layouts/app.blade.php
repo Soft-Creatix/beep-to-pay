a3eb59abc88328f991ce7f78bf859f71980048fe
Umer Sethi <umer.zahid1993@gmail.com>
2022-03-03 00:49:11 +0500

    - updated!

--- resources/views/website/layouts/app.blade.php
+++ resources/views/website/layouts/app.blade.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('website.layouts.partials.head')
-
-<body class="bg-grey-made">
-    @include('website.layouts.partials.navbar')
-    @yield('content')
-    @include('website.layouts.partials.footer')
-    @include('website.layouts.partials.scripts')
-
-</body>
-
+    @include('website.layouts.partials.head')
+    <body class="bg-grey-made">
+        @include('website.layouts.partials.navbar')
+        @yield('content')
+        @include('website.layouts.partials.footer')
+        @include('website.layouts.partials.scripts')
+    </body>
 </html>
